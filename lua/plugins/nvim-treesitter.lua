b5572313c6dc9268daa004be3f9d1030b7f3c047
Aiden McCormack <amccormack30@bloomberg.net>
2025-10-28 17:33:52 -0400

    Overhaul nvim config

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,27 +1,27 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  opts = function(_, opts)
-    local function add(lang)
-      if type(opts.ensure_installed) == "table" then
-        table.insert(opts.ensure_installed, lang)
-      end
-    end
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+        local function add(lang)
+            if type(opts.ensure_installed) == "table" then
+                table.insert(opts.ensure_installed, lang)
+            end
+        end
 
-    vim.filetype.add({
-      extension = { rasi = "rasi", rofi = "rasi", wofi = "rasi" },
-      filename = {
-        ["vifmrc"] = "vim",
-      },
-      pattern = {
-        [".*/waybar/config"] = "jsonc",
-        [".*/mako/config"] = "dosini",
-        [".*/kitty/.+%.conf"] = "kitty",
-        [".*/hypr/.+%.conf"] = "hyprlang",
-        ["%.env%.[%w_.-]+"] = "sh",
-      },
-    })
-    vim.treesitter.language.register("bash", "kitty")
+        vim.filetype.add({
+            extension = { rasi = "rasi", rofi = "rasi", wofi = "rasi" },
+            filename = {
+                ["vifmrc"] = "vim",
+            },
+            pattern = {
+                [".*/waybar/config"] = "jsonc",
+                [".*/mako/config"] = "dosini",
+                [".*/kitty/.+%.conf"] = "kitty",
+                [".*/hypr/.+%.conf"] = "hyprlang",
+                ["%.env%.[%w_.-]+"] = "sh",
+            },
+        })
+        vim.treesitter.language.register("bash", "kitty")
 
-    add("git_config")
-  end,
+        add("git_config")
+    end,
 }
