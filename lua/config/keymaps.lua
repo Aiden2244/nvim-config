b5397b047cb9db10adfa6669f5f47ab86b3ae3f8
Aiden McCormack <github.quicken590@passmail.net>
2025-10-29 13:15:29 -0400

    Add keybind for returning to start page

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Return to the start screen
+vim.keymap.set("n", "<leader>uu", function()
+    Snacks.dashboard()
+end, { desc = "Return to Snacks dashboard" })
